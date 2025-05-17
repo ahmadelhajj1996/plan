2f3813c06768a3aa54976cfd400191feac3e0dfc
Ahmad Elhaj <139491035+ahmadelhajj1996@users.noreply.github.com>
2025-05-17 16:39:18 +0300

    Delete src directory

--- src/components/UnauthenticatedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate , Outlet } from 'react-router-dom'
-import { useSelector } from 'react-redux';
-
-const UnauthenticatedRoute = () => {
-  const auth = useSelector((state) => state.test.auth);
-  return !auth ? <Outlet /> : <Navigate to="/" />;
-}
-
-export default UnauthenticatedRoute;
